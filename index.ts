fdb9be6345acef9655f32d9672f87411f6b64eb6
vuongle2609 <vuongle2609@gmail.com>
2022-04-11 15:40:21 +0700

    fix cors

--- index.ts
+++ index.ts
@@ -9,6 +9,12 @@ dotenv.config();
 connect();
 const app = express();
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.json());
 app.use(cors());
 app.use("/", routes);
