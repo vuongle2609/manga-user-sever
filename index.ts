9297191b85f97ed17f65e95fb9b178343deb6191
vuongle2609 <vuongle2609@gmail.com>
2022-04-12 21:48:03 +0700

    fuckyou cors?

--- index.ts
+++ index.ts
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, content-type, Accept, authorization");
   next();
 });
 
